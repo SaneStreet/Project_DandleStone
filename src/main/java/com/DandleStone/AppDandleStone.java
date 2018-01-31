c0260660d2ee180401d485c0963a8e85acfbc8c7
Michael Hansen <michaelhansenit@live.dk>
2018-01-31 10:48:46 +0100

    Initial commit

---
+++ src/main/java/com/DandleStone/AppDandleStone.java
@@ -0,0 +1,26 @@
+package com.DandleStone;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+import com.sun.scenario.Settings;
+
+public class AppDandleStone extends GameApplication{
+
+    //Set the Scene
+    @Override
+    protected void initSettings(GameSettings Settings) {
+        Settings.setHeight(10*70);
+        Settings.setWidth(15*70);
+    }
+
+    //get the Gameworld (TiledMap.json
+    @Override
+    protected void initGame() {
+        getGameWorld().setLevelFromMap("TestMap.json");
+    }
+
+    //Launch the app
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
