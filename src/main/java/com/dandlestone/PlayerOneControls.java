bc7d5709206ed85d56d6ce88cf22bd5bea774f44
kasp4770 <kasp4770@edu.easj.dk>
2018-02-01 20:43:26 +0100

    No doublejump

--- src/main/java/com/dandlestone/PlayerOneControls.java
+++ src/main/java/com/dandlestone/PlayerOneControls.java
@@ -2,11 +2,14 @@ package com.dandlestone;
 
 import com.almasb.fxgl.entity.Control;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.PhysicsComponent;
 
 public class PlayerOneControls extends Control{
 
     private PhysicsComponent physics;
+    private Entity entity;
 
     @Override
     public void onUpdate(Entity entity, double v) {
@@ -21,12 +24,7 @@ public class PlayerOneControls extends Control{
         physics.setVelocityX(150);
     }
 
-    public void wjump() {
-        physics.setVelocityY(-400);
-    }
-
-
-
-
+    //Jump, only from platform
+    public void wjump() { if (physics.getVelocityY() == 0) physics.setVelocityY(-520); }
 
 }
