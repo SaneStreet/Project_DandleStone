38457ab872d62d46375d7e4cdfde858c4b1edcd7
Benjamin <benjamin@smoerfugl.dk>
2018-02-05 13:50:35 +0100

    added sprite animation to the two players with different sprite sets

--- src/main/java/com/dandlestone/PlayerOneControls.java
+++ src/main/java/com/dandlestone/PlayerOneControls.java
@@ -1,26 +1,57 @@
 package com.dandlestone;
 
+import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.entity.Control;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
+
+import javax.xml.datatype.Duration;
 
 public class PlayerOneControls extends Control{
 
     private PhysicsComponent physics;
     private Entity entity;
+    private AnimatedTexture texture;
+    private AnimationChannel animIdle, animRun;
+
+    public PlayerOneControls(){
+
+        animIdle = new AnimationChannel("playerspriteidle.png", 3 , 141/3, 51, javafx.util.Duration.seconds(1), 0, 2);
+        animRun = new AnimationChannel("playerspriterunning.png", 8 , (47+(7/10)), 56, javafx.util.Duration.seconds(1), 0, 7);
+
+        texture = new AnimatedTexture(animIdle);
+
+    }
+
+    @Override
+    public void onAdded(Entity entity) {
+        entity.setView(texture);
+    }
 
     @Override
     public void onUpdate(Entity entity, double v) {
 
+        texture.setAnimationChannel(isMoving() ? animRun : animIdle);
+
+    }
+{
+
+    }
+    private boolean isMoving() {
+        return FXGLMath.abs(physics.getVelocityX()) > 0;
     }
 
     public void aleft() {
+        getEntity().setScaleX(1);
         physics.setVelocityX(-150);
     }
 
     public void dright() {
+        getEntity().setScaleX(-1);
         physics.setVelocityX(150);
     }
 
--- src/main/java/com/dandlestone/PlayerTwoControls.java
+++ src/main/java/com/dandlestone/PlayerTwoControls.java
@@ -1,26 +1,50 @@
 package com.dandlestone;
 
+import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.entity.Control;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
 
 public class PlayerTwoControls extends Control{
 
     private PhysicsComponent physicstwo;
     private Entity entitytwo;
+    private AnimatedTexture texture;
+    private AnimationChannel animIdle, animRun;
+
+    public PlayerTwoControls () {
+        animIdle = new AnimationChannel("playertwospriteidle.png", 3 , 141/3, 51, javafx.util.Duration.seconds(1), 0, 2);
+        animRun = new AnimationChannel("playertwospriterunning.png", 8 , (47+(7/10)), 56, javafx.util.Duration.seconds(1), 0, 7);
+
+        texture = new AnimatedTexture(animIdle);
+    }
 
     @Override
-    public void onUpdate(Entity entitytwo, double v) {
+    public void onAdded(Entity entity) {
+        entity.setView(texture);
 
     }
 
+    private boolean isMoving() {
+        return FXGLMath.abs(physicstwo.getVelocityX()) > 0;
+    }
+
+    @Override
+    public void onUpdate(Entity entitytwo, double v) {
+        texture.setAnimationChannel(isMoving() ? animRun : animIdle);
+    }
+
     public void arrowleft() {
+        getEntity().setScaleX(1);
         physicstwo.setVelocityX(-150);
     }
 
     public void arrowright() {
+        getEntity().setScaleX(-1);
         physicstwo.setVelocityX(150);
     }
 
